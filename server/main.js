f8f4680c4d08067de45ad528a4431c3527fa65ab
sebastian17pepp@gmail.com <fhkwxexkp7cbr9cw>
2017-11-07 15:00:24 +0100

    Lagt til productDB is server, det er en collection hvor alle produktene skal ligge. Samme som en sammling med tables i java server greien

--- server/main.js
+++ server/main.js
@@ -1,1 +1,46 @@
 import '../imports/api/tasks.js';
+
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+
+
+//Produktene ligger i denne her
+export const productsDB = new Mongo.Collection('productsDB');
+
+if(Meteor.isServer){
+    Meteor.publish('productsDB', function publishProducts() {
+        return productsDB.find();
+    })
+}
+
+//Metoder for å legge til produkter
+Meteor.methods({
+    'productsDB.insert'(prodName, prodPrice, prodDate, prodDesc){
+
+        console.log("In insert");
+        check(prodName, String);
+        check(prodDesc, String);
+        check(prodPrice, String);
+        check(prodDate, String);
+
+        if(!Meteor.userId()){
+            throw new Meteor.Error('not authorized');
+        }
+
+        productsDB.insert({
+            name: prodName,
+            price: prodPrice,
+            description: prodDesc,
+            date: prodDate,
+            seller: Meteor.userId(),
+            isAvailable: true
+        });
+        console.log("insert complete");
+    },
+
+    'productsDB.remove'(prodId){
+        check(prodId, String);
+        productsDB.remove(prodId);
+    }
+});
